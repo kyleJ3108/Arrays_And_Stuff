5d914890268f1b7a0453b39e6162d23e1941b814
DevUser <DevUser@002010-095>
2022-09-29 12:38:00 +0100

    Add project files.

---
+++ Arrays_And_Stuff/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays_And_Stuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = addNumbers(10, 5, 1);
+            Console.WriteLine(result);
+        }
+
+        public static int addNumbers(int number1, int number2, int number3)
+        {
+            int result = number1 + number2 + number3;
+            return result;
+            
+        }
+
+    }
+}
