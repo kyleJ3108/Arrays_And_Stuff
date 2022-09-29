764722a28af0c5e3917aaaa7a4d7e15694c8a7a5
C2k-DEV <enniqa@gmail.com>
2022-09-29 12:53:05 +0100

    added nothing

--- Arrays_And_Stuff/Program.cs
+++ Arrays_And_Stuff/Program.cs
@@ -13,7 +13,8 @@ namespace Arrays_And_Stuff
         public static int addNumbers(int number1, int number2, int number3)
         {
             int result = number1 + number2 + number3;
-            return result;
+            return result;c#
+                jasdasdasdsefghasdasdjfgbl
             
         }
 
